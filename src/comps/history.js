16085a548348f811da4013afc43650ddca461022
TehilaRotenberg <89943199+TehilaRotenberg@users.noreply.github.com>
2023-08-27 13:44:11 +0300

    Revert "final commit"

--- src/comps/history.js
+++ src/comps/history.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import UserContex from '../contex'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
@@ -7,21 +7,16 @@ export default function History() {
     const {history,setHistory,History,getDefulteValue}=useContext(UserContex);
     const navigate = useNavigate();
 
-    useEffect(()=>{
-      console.log(history);
-    },[])
-
-    useEffect(()=>{
-      console.log(history);
-    },[history])
-
     const deleteFromHistory=(i)=>{
-     setHistory(prev=>prev.filter((_item,index)=>index!==i))
+     let historySearch=history;
+     console.log(historySearch[i]);
+     historySearch.splice(i,1);
+     setHistory(historySearch)
 
      if (history.length<1) {
            getDefulteValue()
      }
-     console.log(history);
+     
     }
 
     const deselect=()=>{
@@ -43,7 +38,7 @@ export default function History() {
             <th className='td'>יבשת</th>
             <th className='td'>פעולות</th>
           </tr>
-          {(history.length>0)&&
+          {
             history.map((item,index)=>
             <tr key={index}>
             <td className='td'>{item.city.city}</td>
@@ -53,7 +48,7 @@ export default function History() {
                 <ul className='header--ul'> 
                     {(item.city.city==="Jerusalem")&&<li className='header--li' onClick={()=>{selectMainPage(index)}}><Link className='link'>הפוך לראשי</Link></li>}
                     {(index==history.length-1)&&<li className='header--li'>
-                        <Link className='link activ--link' onClick={()=>deselect()}>ביטול בחירה</Link>
+                        <Link className='link activ--link' onClick={deselect()}>ביטול בחירה</Link>
                     </li>}
                     <li className='header--li'  onClick={()=>{deleteFromHistory(index)}}>
                       מחיקה מההיסטוריה
